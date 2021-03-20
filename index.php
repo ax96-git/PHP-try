8d563330de45c079c085aa7a0b010e1fc84d95d2
mohammed <70203540+ax96-git@users.noreply.github.com>
2021-03-20 10:00:09 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,26 @@
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>php try</title>
+	<link rel="stylesheet" type="text/css" href="phptry.css">
+</head>
+<body>
+	<h3>Welome to PHP</h3>
+	<?php 
+	$feel='Baby';
+	$BY='1996';
+	$PY=date('Y');
+
+	$age=($PY-$BY);
+	?>
+<h3><?php print "Arbaz" . "Ali" . "<br>";
+	echo 'love';?>
+</h3>
+<iframe src="https://drive.google.com/file/d/1LYFL8pjqW9EkBQWCGtughbgr8k3VrOw5/preview" width="640" height="480"></iframe>
+<h3><?php echo "$feel from londen"; ?></h3>
+<h3><?php echo "$age";?></h3>
+<iframe src="https://onedrive.live.com/embed?cid=E0665365CD8631F2&resid=E0665365CD8631F2%21803&authkey=ABbfC1tn4bVftvc" width="320" height="180" frameborder="0" scrolling="no" allowfullscreen></iframe>
+
+</body>
+</html>
